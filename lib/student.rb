01eda3e647254645ebc342958acb940aef81c6fc
Nate Miller <natekmiller96@gmail.com>
2018-07-27 21:56:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,4 +16,9 @@ class Student
           SQL
       DB[:conn].execute(sql)
   end
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE IF EXISTS students
+        SQL
+    DB[:conn].execute(sql)
 end
